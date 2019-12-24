1dc798fa3e102033882e02c997bfafca158abdd7
YuriiKniazyk <y.kniazyk@gmail.com>
2019-12-24 17:29:44 +0200

    add products to shopify

--- app.js
+++ app.js
@@ -6,11 +6,15 @@ const cookie = require('cookie');
 const nonce = require('nonce')();
 const querystring = require('querystring');
 const request = require('request-promise');
+const fs = require('fs');
 
 const apiKey = process.env.SHOPIFY_API_KEY;
 const apiSecret = process.env.SHOPIFY_API_SECRET;
 const scopes = 'write_products';
-const forwardingAddress = "https://4310e805.ngrok.io"; // Replace this with your HTTPS Forwarding address
+const forwardingAddress = "https://94ada2d7.ngrok.io";
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/shopify', (req, res) => {
     const shop = req.query.shop;
@@ -58,8 +62,7 @@ app.get('/shopify/callback', (req, res) => {
             hashEquals = crypto.timingSafeEqual(generatedHash, providedHmac)
         } catch (e) {
             hashEquals = false;
-        };
-
+        }
         if (!hashEquals) {
             return res.status(400).send('HMAC validation failed');
         }
@@ -75,6 +78,13 @@ app.get('/shopify/callback', (req, res) => {
         request.post(accessTokenRequestUrl, { json: accessTokenPayload })
             .then((accessTokenResponse) => {
                 const accessToken = accessTokenResponse.access_token;
+                fs.writeFile("accessToken.txt", accessToken, function(error){
+
+                    if(error) throw error; // если возникла ошибка
+                    console.log("Write file done. Content:");
+                    let data = fs.readFileSync("accessToken.txt", "utf8");
+                    console.log(data);  // выводим считанные данные
+                });
                 // DONE: Use access token to make API call to 'shop' endpoint
                 const shopRequestUrl = 'https://' + shop + '/admin/api/2019-10/products.json';
                 const shopRequestHeaders = {
@@ -96,6 +106,49 @@ app.get('/shopify/callback', (req, res) => {
     } else {
         res.status(400).send('Required parameters missing');
     }
+});
+
+app.post('/app/create-product', function (req, res) {
+    let new_product = {
+        product: {
+            title: req.body.title,
+            body_html: req.body.body_html,
+            vendor: req.body.vendor,
+            product_type: req.body.product_type,
+            tags: req.body.tags
+        }
+    };
+    let url = 'https://' + req.query.shop + '/admin/products.json';
+
+    let options = {
+        method: 'POST',
+        uri: url,
+        json: true,
+        resolveWithFullResponse: true,//added this to view status code
+        headers: {
+            'X-Shopify-Access-Token': process.env.appStoreTokenTest,
+            'content-type': 'application/json'
+        },
+        body: new_product//pass new product object - NEW - request-promise problably updated
+    };
+
+    request.post(options)
+        .then(function (response) {
+            console.log(response.body);
+            if (response.statusCode == 201) {
+                res.json(true);
+            } else {
+                res.json(false);
+            }
+        })
+        .catch(function (err) {
+            console.log(err);
+            res.json(false);
+        });
+});
+
+app.get('*', (req, res) => {
+    res.status(404).json('Page not found!!!');
 });
 
 app.listen(3000, () => {
